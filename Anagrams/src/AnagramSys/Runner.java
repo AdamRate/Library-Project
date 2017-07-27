cf36dd29d5b02795cb5e62ef85a1d90121c6fde9
Adam <adamjrate@hotmail.co.uk>
2017-07-27 17:12:26 +0000

    Finished big List Anagrams

--- Anagrams/src/AnagramSys/Runner.java
+++ Anagrams/src/AnagramSys/Runner.java
@@ -1,10 +1,15 @@
 package AnagramSys;
 
+import java.util.ArrayList;
+
 public class Runner {
 	public static void main(String[] args) {
-
 	input Input = new input();	
-	Input.sortArray(Input.inputFromFile());
-	System.out.println(Input.sortWord("TESTING"));
+	
+	//ArrayList<String> wordArray = Input.sortArray(Input.inputFromFile());
+	//System.out.println(Input.sortWord("TESTING"));
+	Input.Process(Input.sortArray(Input.inputFromFile()));
+	
+	//Input.Process(wordArray);
 	}
 }
--- Anagrams/src/AnagramSys/input.java
+++ Anagrams/src/AnagramSys/input.java
@@ -7,13 +7,17 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class input {
 
-	public  ArrayList<String> inputFromFile() {
+	public ArrayList<String> inputFromFile() {
+
 		BufferedReader br = null;
 		FileReader fr = null;
-		String FILENAME = "Anagrams.txt";
+		String FILENAME = "wordList.txt";
 		try {
 			fr = new FileReader(FILENAME);
 
@@ -28,14 +32,14 @@ public class input {
 			while ((sCurrentLine = br.readLine()) != null) {
 				wordArray.add(sCurrentLine);
 			}
-			System.out.println(wordArray);
+			// System.out.println(wordArray);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		return wordArray;
 	}
 
-	public  void sortArray(ArrayList<String> wordArray) {
+	public ArrayList<String> sortArray(ArrayList<String> wordArray) {
 
 		for (int i = 0; i < wordArray.size(); i++) {
 			String test = wordArray.get(i);
@@ -44,24 +48,43 @@ public class input {
 			String sorted = String.valueOf(ar);
 			wordArray.set(i, sorted);
 		}
-		System.out.println(wordArray);
+		return wordArray;
 	}
 
 	public String sortWord(String Input) {
 		char[] ar = Input.toCharArray();
 		Arrays.sort(ar);
 		String sorted = String.valueOf(ar);
-
 		return sorted;
 	}
-	
-	public void Process(String sortedValue){
-		int i=0;
+
+	public void Process(ArrayList<String> sortedValue) {
+		int j = 0;
+		int v = 0;
+		int Biggest = 0;
+		String BiggestEntry = "";
+
+		Entry<String, Integer> entry = null;
 		HashMap<String, Integer> map = new HashMap<>();
-		map.put(sortedValue, i);
-		if (map.get(sortedValue) == null){
-	//	int i1 =	map.entrySet("1");
+		for (j = 0; j < sortedValue.size(); j++)
+			if (map.containsKey(sortedValue.get(j))) {
+				v = map.get(sortedValue.get(j));
+				v++;
+				map.put(sortedValue.get(j), v);
+			} else
+				map.put(sortedValue.get(j), 0);
+
+		Iterator<Entry<String, Integer>> it = map.entrySet().iterator();
+		while (it.hasNext()) {
+			entry = it.next();
+
+			if (entry.getValue() > Biggest) {
+				Biggest = entry.getValue();
+				BiggestEntry = entry.getKey();
+			}
 		}
-		
+		// System.out.println(map.size());
+		System.out.println("Most anagrams is: " + BiggestEntry + " With : " + Biggest + " Instances");
 	}
+
 }
